53924aee6c558f6dd71283af2be08a0b4b828263
hadesee <rlagusrl0606@naver.com>
2026-03-11 00:58:27 +0900

    260311 desktop

--- graph/1240.cpp
+++ graph/1240.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 
 using namespace std;
-
+/*시간이 많이 소요된 문제 아마도 bfs dfs의 문제라기 보단 갈 수 있는 길을 찾는 과정에서 벌어지는 시간 소요때문으로 추측 즉 배열로 구성했기때문에 */
 int graph[1001][1001] = {0};
 
-void dfs(int s, int d);
+void dfs(int s, int d, int arr[]);
 int a, b, c;
+int n, m, token = 0;
 
 int main()
 {
-    int n, m;
-    
-
+    ios::sync_with_stdio(0), cin.tie(0);
     cin >> n >> m;
 
     for (int i = 1; i < n; i++)
@@ -20,19 +19,45 @@ int main()
         graph[a][b] = c;
         graph[b][a] = c;
     }
-    
+
     for (int i = 0; i < m; i++)
     {
-        c=0;
+        int check[1001] = {0};
+        c = 0;
         cin >> a >> b;
-        dfs(a,b);
+        check[a] = 1;
+        dfs(a, b, check);
+        token--;
     }
-    
-    
 }
 
-void dfs(int s, int d)
+void dfs(int s, int d, int arr[])
 {
-    
+    for (int i = 1; i <= n; i++)
+    {
+        if (graph[s][i] != 0 && arr[i] == 0)
+        {
+            arr[i]++;
+            c += graph[s][i];
 
+            if (i == d)
+            {
+                cout << c << '\n';
+                token = 1;
+                return;
+            }
+            
+
+            dfs(i, d, arr);
+
+            if (token == 1)
+            {
+                return;
+            }
+
+            c -= graph[s][i];
+        }
+    }
+
+    return;
 }
